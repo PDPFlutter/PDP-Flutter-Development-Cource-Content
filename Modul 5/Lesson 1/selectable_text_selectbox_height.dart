b714d1759ac3fef2f86ea17cfee6c21a3b315a03
PDPFlutter <pdpflutter@gmail.com>
2024-10-14 17:47:56 +0500

    Create selectable_text_selectbox_height.dart

---
+++ Modul 5/Lesson 1/selectable_text_selectbox_height.dart
@@ -0,0 +1,51 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: BoxHeightStyle.values.map((boxHeightStyle) {
+              return  Column(
+                children: [
+                  SelectableText(
+                    "$boxHeightStyle",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      height: 2.5,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 27.5,
+                    ),
+                    selectionHeightStyle: boxHeightStyle,
+                  ),
+                  const SizedBox(height: 5,),
+                ],
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
