8f737aac015e9b525bd9ed450275735aa9191af8
PDPFlutter <pdpflutter@gmail.com>
2024-10-14 17:46:08 +0500

    Create selectable_text_max_min_line.dart

---
+++ Modul 5/Lesson 1/selectable_text_max_min_line.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SelectableText(
+            'SelectableText class - a run of selectable text with a single style. The SelectableText widget displays a string of text with a single style. The string might break across multiple lines or might all be displayed on the same line depending on the layout constraints.',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+            ),
+            minLines: 2,
+            maxLines: 4,
+          ),
+        ),
+      ),
+    );
+  }
+}
