da0b1a1622f754c7e11458e14b5ce1bbb4cfca5e
PDPFlutter <pdpflutter@gmail.com>
2024-10-20 14:45:59 +0500

    Create dismissible_example_one.dart

---
+++ Modul 5/Lesson 2/dismissible_example_one.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final items = List<String>.generate(20, (i) => 'Item ${i + 1}');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dismissing Items'),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+          return Dismissible(
+            key: Key(item),
+            onDismissed: (direction) {
+              setState(() {
+                items.removeAt(index);
+              });
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('$item dismissed')));
+            },
+            background: Container(color: Colors.red),
+            child: ListTile(
+              title: Text(item),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
