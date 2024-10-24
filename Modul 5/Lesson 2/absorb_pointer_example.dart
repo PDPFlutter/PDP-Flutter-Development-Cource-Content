3aa6b47bee6637f9231982f4a5dbba04849cd5ae
PDPFlutter <pdpflutter@gmail.com>
2024-10-24 07:31:14 +0500

    Create absorb_pointer_example.dart

---
+++ Modul 5/Lesson 2/absorb_pointer_example.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Absorb Pointer")),
+      body: Center(
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: <Widget>[
+            SizedBox(
+              width: 200.0,
+              height: 100.0,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: null,
+              ),
+            ),
+            SizedBox(
+              width: 100.0,
+              height: 200.0,
+              child: AbsorbPointer(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue.shade200,
+                  ),
+                  onPressed: () {},
+                  child: null,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
