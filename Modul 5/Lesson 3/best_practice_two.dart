dd5cdb2e715ac16efa01366e9c258eb78460cd91
PDPFlutter <pdpflutter@gmail.com>
2024-11-01 10:31:21 +0500

    Create best_practice_two.dart

---
+++ Modul 5/Lesson 3/best_practice_two.dart
@@ -0,0 +1,91 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      home: StickyDemo(title: "Best Practice"),
+    ));
+
+class StickyDemo extends StatefulWidget {
+  final String title;
+
+  const StickyDemo({super.key, required this.title});
+
+  @override
+  State<StickyDemo> createState() => _StickyDemoState();
+}
+
+class _StickyDemoState extends State<StickyDemo> with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            elevation: 0,
+            expandedHeight: 250,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(widget.title),
+              background: Image.network(
+                'http://img1.mukewang.com/5c18cf540001ac8206000338.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: StickyTabBarDelegate(
+              child: TabBar(
+                labelColor: Colors.black,
+                controller: tabController,
+                tabs: const <Widget>[
+                  Tab(text: 'Home'),
+                  Tab(text: 'Profile'),
+                ],
+              ),
+            ),
+          ),
+          SliverFillRemaining(
+            child: TabBarView(
+              controller: tabController,
+              children: const <Widget>[
+                Center(child: Text('Content of Home')),
+                Center(child: Text('Content of Profile')),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
+  final TabBar child;
+
+  StickyTabBarDelegate({required this.child});
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return child;
+  }
+
+  @override
+  double get maxExtent => child.preferredSize.height;
+
+  @override
+  double get minExtent => child.preferredSize.height;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
