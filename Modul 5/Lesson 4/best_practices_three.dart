c3ef4f04f37ea7211851dbc6ad5be660c1674984
PDPFlutter <pdpflutter@gmail.com>
2024-11-07 21:33:21 +0500

    Create best_practices_three.dart

---
+++ Modul 5/Lesson 4/best_practices_three.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: const HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+        ),
+        child: CustomPaint(
+          painter: BackgroundPainter(),
+        ),
+      ),
+    );
+  }
+}
+
+class BackgroundPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = Colors.white.withOpacity(0.1);
+    final path = Path();
+
+    path.moveTo(0, 0);
+    path.lineTo(size.width * 0.2, 0);
+    path.lineTo(0, size.height * 0.4);
+    path.close();
+
+    final path2 = Path();
+
+    path2.moveTo(size.width, 0);
+    path2.lineTo(size.width * 0.8, 0);
+    path2.lineTo(size.width * 0.2, size.height);
+    path2.lineTo(size.width, size.height);
+    path2.close();
+
+    canvas.drawPath(path, paint);
+    canvas.drawPath(path2, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
