622c54c7bee4facf97d18a49cc18ec48b4313ace
PDPFlutter <pdpflutter@gmail.com>
2024-11-07 21:39:53 +0500

    Create clip_oval.dart

---
+++ Modul 5/Lesson 4/clip_oval.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DemoApp());
+}
+
+class DemoApp extends StatelessWidget {
+  const DemoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DemoPage(),
+    );
+  }
+}
+
+class DemoPage extends StatefulWidget {
+  const DemoPage({super.key});
+
+  @override
+  State<DemoPage> createState() => _DemoPageState();
+}
+
+class _DemoPageState extends State<DemoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ClipOval(
+          clipper: MyClip(),
+          child: Image.network(
+            "https://plus.unsplash.com/premium_photo-1682621096396-d250bd8e9256?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyClip extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) {
+    return const Rect.fromLTWH(25, 130, 380, 400);
+  }
+
+  @override
+  bool shouldReclip(oldClipper) {
+    return this != oldClipper;
+  }
+}
