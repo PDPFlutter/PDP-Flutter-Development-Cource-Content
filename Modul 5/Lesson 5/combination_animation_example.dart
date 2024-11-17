17feddc68e8b83cb4453bdaf6cb1a21b9ddd06f9
PDPFlutter <pdpflutter@gmail.com>
2024-11-17 08:12:54 +0500

    Create combination_animation_example.dart

---
+++ Modul 5/Lesson 5/combination_animation_example.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedFlutterLogo(
+          animation: controller,
+          child: const FlutterLogo(),
+        ),
+      ),
+    );
+  }
+}
+
+class AnimatedFlutterLogo extends AnimatedWidget {
+  final Widget child;
+
+  const AnimatedFlutterLogo({
+    super.key,
+    required Animation<double> animation,
+    required this.child,
+  }) : super(listenable: animation);
+
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Opacity(
+      opacity: _opacityTween.evaluate(animation),
+      child: SizedBox(
+        height: _sizeTween.evaluate(animation),
+        width: _sizeTween.evaluate(animation),
+        child: child,
+      ),
+    );
+  }
+}
