a964d02f5638a65657adda6a59ba5d5a325980fa
PDPFlutter <pdpflutter@gmail.com>
2025-01-19 15:35:50 +0500

    Update network_service_one.dart

--- Modul 6/Lesson 7/network_service_one.dart
+++ Modul 6/Lesson 7/network_service_one.dart
@@ -8,53 +8,37 @@ sealed class Network {
   };
 
   static Future<String?> methodGet({required String api, Object? id}) async {
-    try {
-      Uri url = Uri.https(baseUrl, "$api${id != null ? "/$id" : ""}");
-      final response = await get(url, headers: headers);
-      if(response.statusCode == 200) {
-        return utf8.decoder.convert(response.bodyBytes);
-      }
-    } catch(e) {
-      return null;
+    Uri url = Uri.https(baseUrl, "$api${id != null ? "/$id" : ""}");
+    final response = await get(url, headers: headers);
+    if (response.statusCode == 200) {
+      return utf8.decoder.convert(response.bodyBytes);
     }
     return null;
   }
 
   static Future<String?> methodDelete({required String api, required Object id}) async {
-    try {
-      Uri url = Uri.https(baseUrl, "$api/$id");
-      final response = await delete(url);
-      if(response.statusCode == 200) {
-        return utf8.decoder.convert(response.bodyBytes);
-      }
-    } catch(e) {
-      return null;
+    Uri url = Uri.https(baseUrl, "$api/$id");
+    final response = await delete(url);
+    if (response.statusCode == 200) {
+      return utf8.decoder.convert(response.bodyBytes);
     }
     return null;
   }
 
   static Future<String?> methodPost({required String api, required Map<String, Object?> data}) async {
-    try {
-      Uri url = Uri.https(baseUrl, api);
-      final response = await post(url, body: jsonEncode(data));
-      if(response.statusCode == 200 || response.statusCode == 201) {
-        return utf8.decoder.convert(response.bodyBytes);
-      }
-    } catch(e) {
-      return null;
+    Uri url = Uri.https(baseUrl, api);
+    final response = await post(url, body: jsonEncode(data));
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return utf8.decoder.convert(response.bodyBytes);
     }
     return null;
   }
 
   static Future<String?> methodPut({required String api, required Object id, required Map<String, Object?> data}) async {
-    try {
-      Uri url = Uri.https(baseUrl, "$api/$id");
-      final response = await put(url, body: jsonEncode(data));
-      if(response.statusCode == 200 || response.statusCode == 201) {
-        return utf8.decoder.convert(response.bodyBytes);
-      }
-    } catch(e) {
-      return null;
+    Uri url = Uri.https(baseUrl, "$api/$id");
+    final response = await put(url, body: jsonEncode(data));
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return utf8.decoder.convert(response.bodyBytes);
     }
     return null;
   }
