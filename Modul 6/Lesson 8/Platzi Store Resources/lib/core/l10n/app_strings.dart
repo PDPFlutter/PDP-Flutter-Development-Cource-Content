ad17de2e5726a1b1c3d043b42c7a71a320be52cc
PDPFlutter <pdpflutter@gmail.com>
2025-01-23 14:35:49 +0500

    Create app_strings.dart

---
+++ Modul 6/Lesson 8/Platzi Store Resources/lib/core/l10n/app_strings.dart
@@ -0,0 +1,13 @@
+sealed class AppStrings {
+  static const home = "Home";
+  static const catalog = "Catalog";
+  static const cart = "Cart";
+  static const favorite = "Favorite";
+  static const profile = "Profile";
+
+  static const appName = "Platzi";
+  static const product = "Product";
+  static const description = "Description";
+  static const addToCart = "Add to cart";
+  static const similarProducts = "Similar products";
+}
