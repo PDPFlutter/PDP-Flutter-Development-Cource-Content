f647ac76dcfccd56fabb7588a87ec22313d1ae59
PDPFlutter <pdpflutter@gmail.com>
2025-01-23 14:39:03 +0500

    Create app_colors.dart

---
+++ Modul 6/Lesson 8/Platzi Store Resources/lib/core/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const transparent = Colors.transparent;
+  
+  // Custom Colors
+  static const borderColor = Color(0xFFE3E5E5);
+  
+  // Gray Shades
+  static final grey200 = Colors.grey.shade200;
+  
+  // Random Colors for placeholders
+  static List<Color> get randomColors => Colors.primaries;
+}
